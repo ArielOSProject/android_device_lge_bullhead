864fa5f4d5cd91ff1c04029deebe6d532613dcac
mikalackis <mikalackis@gmail.com>
2019-03-13 11:08:45 +0100

    Ariel bringup

--- lineage.mk
+++ lineage.mk
@@ -8,8 +8,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
+# Inherit some common stuff.
+$(call inherit-product, vendor/ariel/config/ariel_common.mk)
+
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_bullhead
+PRODUCT_NAME := ariel_bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
